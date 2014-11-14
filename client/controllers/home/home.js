a5ef15e7e133bcc820985f095f4718b765fbff87
pouncealot <spamtoheck@gmail.com>
2014-11-14 15:42:24 -0800

    bower

---
+++ client/controllers/home/home.js
@@ -0,0 +1,12 @@
+;(function(){
+'use strict';
+
+  angular
+    .module('home',[])
+    .controller('HomeController', HomeController);
+
+    /* @inject */
+    function HomeController($scope, $location, $window) {
+    }
+
+}).call(this);
